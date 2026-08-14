cf0c1ef2951e0b34421768a4bbe51bff244f1927
Ekky-VTT <83665201+Ekky-VTT@users.noreply.github.com>
2026-08-14 17:56:50 +0200

    Update item-data.mjs

--- module/data/item-data.mjs
+++ module/data/item-data.mjs
@@ -1,4 +1,4 @@
-const { NumberField, StringField, HTMLField } = foundry.data.fields;
+const { NumberField, StringField, HTMLField, BooleanField } = foundry.data.fields;
 
 /**
  * Modèle de données pour les Capacités
@@ -7,7 +7,9 @@ export class CapaciteData extends foundry.abstract.TypeDataModel {
   static defineSchema() {
     return {
       valeur: new NumberField({ initial: 0, min: 0, max: 15, integer: true }),
-      attributLie: new StringField({ initial: "caractere" }), // caractere, discernement, maitrise, prestance, robustesse, vigueur, aucun
+      attributLie: new StringField({ initial: "caractere" }),
+      fortune: new BooleanField({ initial: false }),
+      adversite: new BooleanField({ initial: false }),
       description: new HTMLField({ initial: "" })
     };
   }
