6da4f87fff0cdbb6fd110788a9de93491018f06a
Ekky-VTT <83665201+Ekky-VTT@users.noreply.github.com>
2026-08-14 16:10:17 +0200

    Create lore-and-legacy.mjs

---
+++ module/lore-and-legacy.mjs
@@ -0,0 +1,9 @@
+/**
+ * Fichier d'initialisation du système Lore & Legacy
+ */
+
+Hooks.once("init", async function() {
+  console.log("Lore & Legacy | Initialisation du système Lore & Legacy");
+  
+  // Nous ajouterons ici plus tard la configuration de nos classes (Actor, Item)
+});
